d3cb92139b6965c9e7e58d2c698b1307dac3cf88
Noah Naiman <nnaiman@bu.edu>
2017-12-02 17:42:57 -0500

    Working on jquery.

--- background.js
+++ background.js
@@ -1,5 +1,4 @@
 var s = document.createElement('script');
-console.log("Heyo");
 // TODO: add "script.js" to web_accessible_resources in manifest.json
 s.src = chrome.extension.getURL('index.js');
 s.onload = function() {
--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-console.log(document);
-$('*').click(function(e){
-	console.log("Hello!");
+//$.noConflict();
+// require('jquery')
+// if (!jQuery) {
+// 	$ = require('jQuery')
+// }
+// console.log($(document).ready(function(){}))
+$(document).ready(function() {
+    console.log(document);
+
+	 $('div').click(function(e){
+		var $target = $(e.currentTarget);
+	// 	// if($target.length == 1){
+	// 	// 	console.log($target);
+	// 	// }
+		console.log($target.children());
+	// 	console.log($target.children().find('a').attr('href'));
+	// 	// while($target.length){
+	// 	// 	$target = $target.children();
+	// 	// }
+	// 	// console.log($target.end().attr('id'));
+	 });
+	// document.onClick = function(){
+	// 	console.log(document);
+	// };
 });
-// document.onClick = function(){
-// 	console.log(document);
-// };
