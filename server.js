9abb71a8111f32119bc099650f1558fc9fc94a74
Noah Naiman <nnaiman@bu.edu>
2017-12-02 02:29:38 -0500

    Created manifest.json for chrome extension.

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 const DataPoint = require('data-point');
 const dataPoint = DataPoint.create();
+
+dataPoint.addEntities({
+	//remote request of CNN stories
+	'request:getCNN':{
+		//{value.date} injects value from the accumulator
+		url: 'https://services.cnn.com/newsgraph/docs/lastPublishDate:{value.date}'
+	}
+
+});
