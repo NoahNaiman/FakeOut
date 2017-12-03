462722a88900ac3291237bfa99559a14c3b657d9
Noah Naiman <nnaiman@bu.edu>
2017-12-03 00:37:48 -0500

    Fixed multi-request issue.

--- extension/background.js
+++ extension/background.js
@@ -2,23 +2,25 @@ $(document).ready(function() {
 	var http = new XMLHttpRequest();
 	var url = 'https://localhost:5000/';
 	var articles = [];
-	 $('*').click(function(e){
+	var requested = false;
+	var i = 0;
+	 $('div').click(function(e){
 		var $target = $(e.currentTarget);
 		$target.find('a').each(function(){
 			var link = String($(this).attr('href'));
-			if(link.indexOf('www.facebook.com') == -1 && link.indexOf('l.facebook.com') == -1 && link.indexOf('developers.facebook.com') == -1 && link != '#' && link.indexOf('http') > -1){
-				articles.push(link);
+			if(link.indexOf('www.facebook.com') == -1 && link.indexOf('l.facebook.com') == -1 && link.indexOf('developers.facebook.com') == -1 && link != '#' && link.indexOf('http') > -1 && link != ''){
+				if(!requested){
+					http.open('POST', url, true);
+					http.send(link);
+					console.log(http.responseText);
+					http.onreadystatechange = function() {
+						if(http.readyState == 4 && http.status == 200){
+							console.log(http.responseText);
+						}
+					}
+					requested = true;
+				}
 			}
 		});
-		articles = Array.from(new Set(articles));
-		http.open('POST', url, true);
-		//http.setReaquestHeader('Content-type', 'application/x-www-form-urlencoded');
-		http.send(articles[0]);
-		console.log(articles[0]);
-	 	// var form = document.createElement('form');
-	 	// form.method = 'POST';
-	 	// form.action = 'http://localhost:5000/';
-	 	// form.style.visibility = 'hidden';
-
 	 });
 });
